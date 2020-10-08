e12b05b48b280fd3d351a6367b9e465e7eb73aca
Leo <leonard.teschner@googlemail.com>
2020-10-08 13:52:30 +0200

    Registered Models to the admin-page

--- LibraryManagement/admin.py
+++ LibraryManagement/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from LibraryManagement.models import *
 
 # Register your models here.
+admin.site.register(Loan)
+admin.site.register(PermLoan)
+admin.site.register(TempLoan)
+admin.site.register(Author)
+admin.site.register(LoanSubject)
+admin.site.register(Book)
+admin.site.register(Material)
+admin.site.register(Device)
+admin.site.register(Container)
+
+
