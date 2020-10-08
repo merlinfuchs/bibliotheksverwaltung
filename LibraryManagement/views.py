5f96d010497db927a281eac56100b7f68a053b7e
Jan <jan1748@schule.bremen.de>
2020-10-08 14:27:52 +0200

    Checking login data for Authentication

--- LibraryManagement/views.py
+++ LibraryManagement/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 
 
 def login(request):
     if request.method == 'POST':
-        print(request.POST)
-
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            print('User', username, 'Logged in')
+            # TODO Create Session
+        else:
+            print('Authentication failed for User:', username)
     return render(request, 'login.html', {})
 
 
