8ac8eaaef86fb7843f6748e778ebc790e48f3ed7
Zlimca <goldslimelp@gmail.com>
2020-11-12 14:36:26 +0100

    Filter

--- LibraryManagement/views.py
+++ LibraryManagement/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from LibraryManagement.models import Book, Material, Device, Container
+from itertools import chain
 
 
 def login(request):
@@ -7,17 +8,34 @@ def login(request):
 
 
 def overview(request):
-    books = Book.objects.all()
-    materials = Material.objects.all()
-    devices = Device.objects.all()
-    containers = Container.objects.all()
+    name_filter = {}
+    name = request.GET.get('name_field')
+    if name:
+        name_filter['name'] = name
 
-    return render(request, 'overview.html', {
+    book_filter = {}
+    subject = request.GET.get('book_subject')
+    if subject:
+        book_filter['subject'] = subject
+
+    device_filter = {}
+    device_type = request.GET.get('type_of_device')
+    if device_type:
+        device_filter['device_type'] = device_type
+
+    books = Book.objects.filter(**name_filter, **book_filter)
+    materials = Material.objects.filter(**name_filter)
+    devices = Device.objects.filter(**name_filter, **device_filter)
+    containers = Container.objects.filter(**name_filter)
+
+    context = {
         "books": books,
         "materials": materials,
         "devices": devices,
         "containers": containers
-    })
+    }
+
+    return render(request, 'overview.html', context)
 
 
 def profile(request):
